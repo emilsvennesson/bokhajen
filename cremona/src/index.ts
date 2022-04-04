50e760bb34e19a12e362280380341e618d338fb8
emilsvennesson <emilsvennesson@users.noreply.github.com>
2022-04-04 21:38:49 +0200

    setup rpc client

--- cremona/src/index.ts
+++ cremona/src/index.ts
@@ -1,3 +1,23 @@
-export const getBooks = () => {
-    console.log('hi');
+import { RequestManager, Client, WebSocketTransport } from "@open-rpc/client-js";
+
+const baseUrl = 'wss://admin.abicart.se/backend/jsonrpc/v1'
+const baseParams = {
+    auth: '',
+    language: 'sv',
+    session: '',
+    webshop: '22777'
 }
+
+const transport = new WebSocketTransport(`${baseUrl}?${new URLSearchParams(baseParams).toString()}`);
+const requestManager = new RequestManager([transport]);
+const client = new Client(requestManager);
+
+const getSearchArticles = async(query) => {
+    const result = await client.request({
+        'method': 'Article.search',
+        'params': [query]
+    })
+    return result;
+}
+
+client.close();
