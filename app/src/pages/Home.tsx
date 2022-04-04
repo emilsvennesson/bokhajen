fff9a021639b54b152d2051bc9976417e2d555ab
Daniel Rygaard Rygaard Daniel <danryg@student.chalmers.se>
2022-04-04 15:54:31 +0200

    Added html components to Home page, also added logo

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,5 +1,33 @@
-export default function Home() {
-  return (
-    <div>Home</div>
-  )
+import { Component } from 'react';
+import './css/home.css';
+
+
+
+
+class Home extends Component {
+
+  state={
+
+  };
+
+  render(){
+    return (
+      <div>
+        <header className='homeHeader'>
+          <img className='logo' src={require('../assets/images/bok..png')} />
+        </header>
+        <section>
+          <h2 className="sectionTitle">Most popular books</h2>
+          <div className="popularBooks">
+            <ul className="bookList">
+              
+            </ul>
+          </div>
+        </section>
+      </div>
+
+    )
+  }
 }
+
+export default Home;
