fc4707049c8c3bf871d4f136b99ff25bbbd66fe2
emilsvennesson <emilsvennesson@users.noreply.github.com>
2022-04-04 23:28:43 +0200

    implement getBook, search

--- cremona/src/index.ts
+++ cremona/src/index.ts
@@ -1,5 +1,7 @@
 import { RequestManager, Client, WebSocketTransport } from "@open-rpc/client-js";
 
+import { Book } from './book.js';
+
 const baseUrl = 'wss://admin.abicart.se/backend/jsonrpc/v1'
 const baseParams = {
     auth: '',
@@ -7,12 +9,11 @@ const baseParams = {
     session: '',
     webshop: '22777'
 }
-
 const transport = new WebSocketTransport(`${baseUrl}?${new URLSearchParams(baseParams).toString()}`);
 const requestManager = new RequestManager([transport]);
 const client = new Client(requestManager);
 
-const getSearchArticles = async(query) => {
+const getSearchArticles = async (query: string) => {
     const result = await client.request({
         'method': 'Article.search',
         'params': [query]
@@ -20,4 +21,51 @@ const getSearchArticles = async(query) => {
     return result;
 }
 
-client.close();
+const parseResult = (result : []) => {
+    const books : Book[] = [];
+    result.forEach((book) => {
+        books.push(new Book(book));
+    })
+    return books;
+}
+
+export const search = async (query: string) => {
+    const searchArticles = await getSearchArticles(query);
+    if (!searchArticles)
+        return []
+
+    const payload = {
+        method: 'Article.list',
+        params: [
+            {
+                'name': { 'sv': true },
+                'articleNumber': true,
+                'uid': true,
+                'url': true,
+                'price': true,
+                'introductionText': { 'sv': true },
+                'images': true,
+                'description': { 'sv': true },
+                'weight': true,
+                'showPricesIncludingVat': true,
+                'attributes': true,
+                'ean': true
+            }
+            , { 'filters': { '/uid': { 'in': searchArticles } } }]
+    }
+
+    const result = await client.request(payload);
+    client.close();
+
+    return parseResult(result);
+}
+
+export const getBook = async (uid: number) => {
+    const payload = {
+        'method': 'Article.get',
+        'params': [uid, true]
+    }
+    const result = await client.request(payload);
+    client.close();
+    return new Book(result);
+}
