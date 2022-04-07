6060aca2d5781ab02fccc56e66004885919ce916
Albin Sundström <albinsundstrom@hotmail.com>
2022-04-07 23:22:54 +0200

    add Cards

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home'
 import NavigationBar from './components/NavigationBar';
+import Cards from './components/Cards';
 import Article from './pages/Article';
 
 function App() {
