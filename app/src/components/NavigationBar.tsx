c212a96fd834b5ba36f1bc4d7b4652f945abc48e
Kexon <kexon@hotmail.se>
2022-04-08 12:13:07 +0200

    Added first iteration of MUI to navbar

--- app/src/components/NavigationBar.tsx
+++ app/src/components/NavigationBar.tsx
@@ -1,9 +1,40 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import './css/NavigationBar.css'
+import { Stack, Button, TextField, ButtonGroup, Box, AppBar, Container, Toolbar} from "@mui/material";
+import App from "../App";
 
 const NavigationBar = () => {
-
     return (
+        <AppBar position='static'>
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                {/* Pre-MUI we used NavLink to navigate, e.g <NavLink to='/'> link </NavLink>
+                    Now we have to import Link from react-router-dom and
+                    MUI Button uses the Link component
+                */}
+                <Button component={Link} to='' >
+                    bok
+                </Button>
+
+                <Button component={Link} to="/article"
+                color='secondary' variant='contained' size='large'>
+                    Köp
+                </Button>
+                <Button component={Link} to="/article"
+                color='secondary' variant='contained' size='large'>
+                    Sälj
+                </Button>
+                <TextField id="outlined-basic" label="Sök" variant="outlined" />
+
+                </Toolbar>
+
+            </Container>
+
+        </AppBar>
+    );
+}
+export default NavigationBar;
+/*
         <nav className="nav">
                            <NavLink className='title'
                 to="/article">bok</NavLink>
@@ -25,7 +56,5 @@ const NavigationBar = () => {
                 to="/article">LOGIN</NavLink>
             </div>
             
-        </nav>
-    );
-}
-export default NavigationBar;
+        </nav>*/
+
