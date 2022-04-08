a2a340682c7a205fe8034e898d1a1088cc254378
Kexon <kexon@hotmail.se>
2022-04-08 16:39:45 +0200

    Reworked search bar

--- app/src/components/NavigationBar.tsx
+++ app/src/components/NavigationBar.tsx
@@ -1,7 +1,39 @@
 import { NavLink, Link } from "react-router-dom";
 import './css/NavigationBar.css'
-import { Stack, Button, TextField, ButtonGroup, Box, AppBar, Container, Toolbar} from "@mui/material";
+import { Stack, Button, TextField, ButtonGroup, Box, AppBar, Container, Toolbar, styled, alpha, InputBase } from "@mui/material";
 import App from "../App";
+
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+}));
+  
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: '12ch',
+        '&:focus': {
+          width: '20ch',
+        },
+      },
+    },
+  }));
 
 const NavigationBar = () => {
     return (
@@ -12,10 +44,13 @@ const NavigationBar = () => {
                     Now we have to import Link from react-router-dom and
                     MUI Button uses the Link component
                 */}
-                <Button component={Link} to='' >
-                    bok
-                </Button>
+                    <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "flex" } }}>
+                        <Button component={Link} to='' color='secondary' >
+                            bok
+                        </Button> 
+                    </Box>
 
+                    <Box sx={{ flexGrow: 5, display: { xs: "none", md: "flex" } }}>
                 <Button component={Link} to="/article"
                 color='secondary' variant='contained' size='large'>
                     Köp
@@ -24,7 +59,17 @@ const NavigationBar = () => {
                 color='secondary' variant='contained' size='large'>
                     Sälj
                 </Button>
-                <TextField id="outlined-basic" label="Sök" variant="outlined" />
+
+                    </Box>
+                    <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+                    <Search>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ 'aria-label': 'search' }}
+                />
+            </Search>
+                        {/*<TextField id="search" label="Sök" variant="outlined" />*/}
+                    </Box>
 
                 </Toolbar>
 
