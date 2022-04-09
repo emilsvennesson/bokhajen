3537c207f0a4bf4603ea146f2a8a0e3fded27c25
emilsvennesson <emilsvennesson@users.noreply.github.com>
2022-04-09 13:14:56 +0200

    initial eslint/prettier config

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home'
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 import NavigationBar from './components/NavigationBar';
 import Article from './pages/Article';
 
@@ -9,8 +9,8 @@ function App() {
     <div className="App">
       <NavigationBar />
       <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/article" element={<Article />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/article" element={<Article />} />
       </Routes>
     </div>
   );
