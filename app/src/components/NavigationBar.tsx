485dd9e13f3dbe50aab10a6f2307c997882d04a3
Kexon <kexon@hotmail.se>
2022-04-11 10:56:20 +0200

    Changed some MUI components

--- app/src/components/NavigationBar.tsx
+++ app/src/components/NavigationBar.tsx
@@ -1,84 +1,108 @@
 import { NavLink, Link } from "react-router-dom";
-import './css/NavigationBar.css'
-import { Stack, Button, TextField, ButtonGroup, Box, AppBar, Container, Toolbar, styled, alpha, InputBase } from "@mui/material";
+import "./css/NavigationBar.css";
+import {
+  Stack,
+  Button,
+  TextField,
+  Typography,
+  Box,
+  AppBar,
+  Container,
+  Toolbar,
+  styled,
+  alpha,
+  InputBase,
+} from "@mui/material";
 import App from "../App";
 
-const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    color: theme.palette.common.black,
-    backgroundColor: alpha(theme.palette.common.white, 1),
-    '&:hover': {
-      backgroundColor: alpha(theme.palette.common.white, 0.95),
-    },
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(0),
-      width: 'auto',
-    },
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  color: theme.palette.common.black,
+  backgroundColor: alpha(theme.palette.common.white, 1),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.95),
+  },
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(0),
+    width: "auto",
+  },
 }));
-  
+
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-      padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-      paddingLeft: `calc(1em + ${theme.spacing(0)})`,
-      transition: theme.transitions.create('width'),
-      width: '100%',
-      [theme.breakpoints.up('sm')]: {
-        width: '20ch',
-        '&:focus': {
-          width: '20ch',
-        },
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(0)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      width: "25ch",
+      "&:focus": {
+        width: "30ch",
       },
     },
-  }));
+  },
+}));
 
 const NavigationBar = () => {
-    return (
-        <AppBar position='static'>
-            <Container maxWidth="xl">
-                <Toolbar disableGutters>
-                {/* Pre-MUI we used NavLink to navigate, e.g <NavLink to='/'> link </NavLink>
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            {/* Pre-MUI we used NavLink to navigate, e.g <NavLink to='/'> link </NavLink>
                     Now we have to import Link from react-router-dom and
                     MUI Button uses the Link component
                 */}
-                    <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "flex" } }}>
-                        <Button component={Link} to='' color='secondary' >
-                            bok
-                        </Button> 
-                    </Box>
+            <Button
+              sx={{ display: { xs: "none", sm: "block" } }}
+              component={Link}
+              to=""
+            >
+              <Typography variant="h6" color="common.white">
+                bok
+              </Typography>
+            </Button>
 
-                    <Box sx={{ flexGrow: 5, display: { xs: "none", md: "flex" } }}>
-                <Button component={Link} to="/article"
-                color='secondary' variant='contained' size='large'>
-                    Köp
-                </Button>
-                <Button component={Link} to="/article"
-                color='secondary' variant='contained' size='large'>
-                    Sälj
-                </Button>
-
-                    </Box>
-                    <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-                        <Search>
-            <StyledInputBase
-              placeholder="Boktitel/ISBN/Kurskod"
-              inputProps={{ 'aria-label': 'search' }}
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+              <Button
+                component={Link}
+                to="/article"
+                color="secondary"
+                variant="contained"
+                size="large"
+              >
+                Köp
+              </Button>
+              <Button
+                component={Link}
+                to="/article"
+                color="secondary"
+                variant="contained"
+                size="large"
+              >
+                Sälj
+              </Button>
+            </Box>
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+              <Search>
+                <StyledInputBase
+                  placeholder="Boktitel/ISBN/Kurskod"
+                  inputProps={{ "aria-label": "search" }}
                 />
-            </Search>
-                        {/*<TextField id="search" label="Sök" variant="outlined" />*/}
-                    </Box>
-
-                </Toolbar>
-
-            </Container>
-
-        </AppBar>
-    );
-}
+              </Search>
+              {/*<TextField id="search" label="Sök" variant="outlined" />*/}
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </Box>
+  );
+};
 export default NavigationBar;
 /*
         <nav className="nav">
@@ -103,4 +127,3 @@ export default NavigationBar;
             </div>
             
         </nav>*/
-
