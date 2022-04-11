96fc2c6d2a3836fa66fa378031feed60fdc5ed63
EliasFalk <elias.falk@hotmail.com>
2022-04-11 11:44:11 +0200

    add mui themeprovider and husky and lint-staged, woof 🐕

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,18 +1,22 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
+import { ThemeProvider } from '@emotion/react';
 import Home from './pages/Home';
 import NavigationBar from './components/NavigationBar';
 import Article from './pages/Article';
+import { mainTheme } from './theme';
 
 function App() {
   return (
-    <div className="App">
-      <NavigationBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/article" element={<Article />} />
-      </Routes>
-    </div>
+    <ThemeProvider theme={mainTheme}>
+      <div className="App">
+        <NavigationBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/article" element={<Article />} />
+        </Routes>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ app/src/theme.tsx
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles';
+
+export const mainTheme = createTheme({
+  palette: {
+    primary: {
+      main: '#0B9E88',
+    },
+    secondary: {
+      main: '#036AAB',
+    },
+  },
+});
