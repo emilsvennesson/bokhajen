77f56b86d3bfc58f63790a56fa5866bcf34687f6
Pouya <kexon@hotmail.se>
2022-04-19 16:02:11 +0200

    Added book image, book title, book description and year

--- app/src/pages/bookdetailview/BookDetailViewDescription.tsx
+++ app/src/pages/bookdetailview/BookDetailViewDescription.tsx
@@ -1,3 +1,4 @@
+import { Box, Container, Stack, Typography } from '@mui/material';
 import { Book } from 'cremona/dist/Book';
 
 interface Props {
@@ -6,5 +7,39 @@ interface Props {
 
 export default function BookDetailViewDescription({ newBook }: Props) {
   const book: Book = newBook;
-  return <div>{book.name}</div>;
+  return (
+    <div>
+      <Box sx={{ display: 'flex', flexDirection: 'row', flex: 1 }}>
+        <Container
+          sx={{
+            flex: 2,
+            display: 'flex',
+            justifyContent: 'center',
+            marginTop: '10%',
+          }}
+        >
+          {/* BOOK IMAGE */}
+          <Stack>
+            <Box component="img" height={300} src={book.image} sx={{}} />
+            <Typography
+              variant="h6"
+              gutterBottom
+              component="div"
+              sx={{ alignSelf: 'center', marginTop: '15px' }}
+            >
+              {book.year}
+            </Typography>
+          </Stack>
+        </Container>
+        <Container sx={{ flex: 3, marginTop: '10%' }}>
+          <Typography variant="h3" gutterBottom component="div">
+            {book.name}
+          </Typography>
+          <Typography variant="h6" gutterBottom component="div">
+            {book.description}
+          </Typography>
+        </Container>
+      </Box>
+    </div>
+  );
 }
