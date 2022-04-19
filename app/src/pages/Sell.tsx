3a0e2f628041c3250b20673492721795cfa43581
Daniel Rygaard <danryg@student.chalmers.se>
2022-04-19 15:51:05 +0200

    Added documentation and Searchbook component

--- app/src/pages/Sell.tsx
+++ app/src/pages/Sell.tsx
@@ -2,14 +2,27 @@ import React from 'react';
 import { Box } from '@mui/material';
 import BookInformationInput from '../components/BookInformationInput';
 import BookConditionChecklist from '../components/BookConditionChecklist';
+import SearchBook from '../components/SearchBook';
 
+/**
+ * Selling page, in this window the user is able to upload a book to the selling page
+ * @returns Sell page
+ */
 export default function Sell() {
+  /**
+   * Placeholder functionfor the Search
+   * @param name
+   */
+  const search = (name: String) => {
+    console.log(name);
+  };
   return (
     <Box
       display="flex"
-      flexDirection="row"
+      flexDirection="column"
       sx={{ backgroundColor: '#C4C4C4', padding: '50px' }}
     >
+      <SearchBook bookSearchHandler={search} />
       <Box flexGrow={2}>
         {/* LEFT Container */}
 
