3e26326469a42b601ecd2b2c728822503a08f4a5
Elias Falk <elias.falk@hotmail.com>
2022-04-19 14:32:02 +0200

    add loading state for auth context

--- app/src/hooks/FBAuthProvider.tsx
+++ app/src/hooks/FBAuthProvider.tsx
@@ -16,9 +16,10 @@ const auth = getAuth();
 
 interface AuthContextType {
   user: User | null;
-  signin: (email: string, password: string) => void;
-  signout: () => void;
-  signup: (email: string, password: string) => void;
+  signin: (email: string, password: string) => Promise<void>;
+  signout: () => Promise<void>;
+  signup: (email: string, password: string) => Promise<void>;
+  loading: boolean;
 }
 
 const AuthContext = React.createContext<AuthContextType>(null!);
@@ -29,41 +30,50 @@ interface Props {
 
 export function FBAuthProvider({ children }: Props) {
   const [user, setUser] = React.useState<User | null>(null);
+  const [loading, setLoading] = React.useState(true);
 
   const signin = async (email: string, password: string) => {
+    setLoading(true);
     try {
       const userCred = await signInWithEmailAndPassword(auth, email, password);
       setUser(userCred.user);
     } catch (e) {
       throw e as Error;
     }
+    setLoading(false);
   };
 
   const signup = async (email: string, password: string) => {
+    setLoading(true);
     try {
-      await createUserWithEmailAndPassword(auth, email, password);
+      const { user: usr } = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password,
+      );
+      setUser(usr);
     } catch (e) {
       throw e as Error;
     }
-    await signin(email, password);
+    setLoading(false);
   };
 
   const signout = async () => {
+    setLoading(true);
     try {
       await signOut(auth);
     } catch (e) {
       throw e as Error;
     }
+    setLoading(false);
   };
 
   useEffect(() => {
+    setLoading(true);
     const unsubscribe = onAuthStateChanged(auth, (usr) => {
-      if (usr) {
-        setUser(usr);
-      } else {
-        setUser(null);
-      }
+      setUser(usr);
     });
+    setLoading(false);
 
     // Cleanup subscription on unmount
     return () => unsubscribe();
@@ -72,7 +82,10 @@ export function FBAuthProvider({ children }: Props) {
   // kinda sus
   // TEMP SOLUTION
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  const value = useMemo(() => ({ user, signin, signout, signup }), [user]);
+  const value = useMemo(
+    () => ({ user, signin, signout, signup, loading }),
+    [user],
+  );
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
