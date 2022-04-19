db79a6528d8d0fabf80edb2bec7ba2b61c21a219
Pouya <kexon@hotmail.se>
2022-04-19 11:30:02 +0200

    Added book props in BookDetailView

--- app/src/pages/bookdetailview/BookDetailView.tsx
+++ app/src/pages/bookdetailview/BookDetailView.tsx
@@ -1,1 +1,10 @@
-export default function BookDetailView() {}
+import { Book } from "cremona/dist/Book";
+
+interface Props {
+    newBook: Book;
+}
+
+export default function BookDetailView({newBook}: Props) {
+    const book: Book = newBook;
+    console.log(book);
+}
