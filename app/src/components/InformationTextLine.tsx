a1bc15d21ba10d6dbdcf7ed2466a2ca6a9f5d87d
DanielRygaard <72791196+Danryg@users.noreply.github.com>
2022-04-20 18:37:10 +0200

    Fixed information text line so that it looks better and has some default values for it's props

--- app/src/components/InformationTextLine.tsx
+++ app/src/components/InformationTextLine.tsx
@@ -1,10 +1,44 @@
-import { Typography } from '@mui/material';
+import { Typography, Box } from '@mui/material';
 import React from 'react';
 
-type Props = {
+interface IInformationTextLineProps {
   children: String;
-};
-
-export default function InformationTextLine({ children }: Props) {
-  return <Typography>{children}</Typography>;
+  label?: string;
+  fontSize?: number;
 }
+
+/**
+ * This is a component that shows a label before some text, this is usaed when you want to display a short line of text that has a sertain meaning. The meaning of the line is specified by the label
+ * @param children This is the text that will be shown after the label and should be a string
+ * @param label? this is the label that will be shwon before the text
+ * @param fontSize? this will set the
+ * @returns InformationTextLine component
+ */
+export default function InformationTextLine({
+  children,
+  label = '',
+  fontSize,
+}: IInformationTextLineProps) {
+  function formatLabel(text: String): JSX.Element {
+    if (text.length === 0) return <Typography />;
+
+    return (
+      <Typography fontSize={fontSize} marginRight={1}>
+        {text}
+      </Typography>
+    );
+  }
+  return (
+    <Box display="flex" flexDirection="row" marginLeft={1}>
+      {formatLabel(label)}
+      <Typography fontSize={fontSize} fontWeight="bold">
+        {children}
+      </Typography>
+    </Box>
+  );
+}
+
+InformationTextLine.defaultProps = {
+  label: '',
+  fontSize: 17,
+};
