66c2538fd451f274e51df9f292adceeeb39ce121
DanielRygaard <72791196+Danryg@users.noreply.github.com>
2022-04-20 18:53:08 +0200

    Added the ability to choose either the text to be bold or the label

--- app/src/components/InformationTextLine.tsx
+++ app/src/components/InformationTextLine.tsx
@@ -5,6 +5,8 @@ interface IInformationTextLineProps {
   children: String;
   label?: string;
   fontSize?: number;
+  labelBold?: boolean;
+  textBold?: boolean;
 }
 
 /**
@@ -12,28 +14,58 @@ interface IInformationTextLineProps {
  * @param children This is the text that will be shown after the label and should be a string
  * @param label? this is the label that will be shwon before the text
  * @param fontSize? this will set the
+ * @param labelBold? if true the label will be shown as bold
+ * @param textBold? if true the text will be shown as bold
  * @returns InformationTextLine component
  */
 export default function InformationTextLine({
   children,
   label = '',
   fontSize,
+  labelBold,
+  textBold,
 }: IInformationTextLineProps) {
+  /**
+   * This formatts the label so that it will be displayed correctly
+   * @param text the label that will be formatted
+   * @returns a specified Typography JSX element
+   */
   function formatLabel(text: String): JSX.Element {
     if (text.length === 0) return <Typography />;
 
+    if (labelBold) {
+      <Typography fontSize={fontSize} fontWeight="bold" marginRight={1}>
+        {text}
+      </Typography>;
+    }
     return (
       <Typography fontSize={fontSize} marginRight={1}>
         {text}
       </Typography>
     );
   }
+
+  /**
+   * This formatts the text so that it will be displayed correctly
+   * @param text the text that will be formatted
+   * @returns a specified Typography JSX element
+   */
+  function formatText(text: String): JSX.Element {
+    if (textBold) {
+      return (
+        <Typography fontSize={fontSize} fontWeight="bold">
+          {text}
+        </Typography>
+      );
+    }
+
+    return <Typography fontSize={fontSize}>{text}</Typography>;
+  }
+
   return (
     <Box display="flex" flexDirection="row" marginLeft={1}>
       {formatLabel(label)}
-      <Typography fontSize={fontSize} fontWeight="bold">
-        {children}
-      </Typography>
+      {formatText(children)}
     </Box>
   );
 }
@@ -41,4 +73,6 @@ export default function InformationTextLine({
 InformationTextLine.defaultProps = {
   label: '',
   fontSize: 17,
+  labelBold: false,
+  textBold: false,
 };
