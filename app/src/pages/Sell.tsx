3f693516c444303e2a0a29af57a132df3600eabf
DanielRygaard <72791196+Danryg@users.noreply.github.com>
2022-04-20 17:45:50 +0200

    Decided to make the book information pop upp instead of needing to scroll down

--- app/src/pages/Sell.tsx
+++ app/src/pages/Sell.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material';
-import BookInformationInput from '../components/BookInformationInput';
-import SearchBook from '../components/SearchBook';
+import { Box, Button, Fade, Typography } from '@mui/material';
+
 import BookInformation from '../components/BookInformation';
 
 /**
@@ -9,58 +8,57 @@ import BookInformation from '../components/BookInformation';
  * @returns Sell page
  */
 export default function Sell() {
-  /**
-   * Placeholder functionfor the Search
-   * @param name
-   */
-  const search = (name: String) => {
-    console.log(name);
-  };
+  const [checked, setChecked] = React.useState(false);
+
   return (
     <Box
       display="flex"
       flexDirection="column"
-      sx={{ backgroundColor: '#C4C4C4', padding: '50px' }}
+      sx={{ backgroundColor: '#C4C4C4' }}
     >
       {/* Title */}
       <Box
         sx={{ backgroundColor: 'white' }}
         marginBottom="40px"
         borderRadius="8px"
+        padding={5}
       >
         <Typography variant="h3" margin="30px">
           Sell your book
         </Typography>
         <Typography variant="h6" margin="20px">
           If you want to publish an add for your book there are three steps you
-          need to go trough
+          need to go trough. First you need to fill out the ISBN number in order
+          for us to know wich book you want to sell
         </Typography>
+        {/* Step 1, look upp your book */}
       </Box>
-
-      {/* Step 1, look upp your book */}
-      <SearchBook bookSearchHandler={search} />
+      <Button onClick={() => setChecked(true)}>Hej</Button>
 
       {/* Step 2, Check information */}
-      <Box display="flex" flexDirection="row" marginTop="40px">
-        <BookInformation
-          name="Mattematisk statistik"
-          edition="4th"
-          year="1994"
-          ISBN="111222233334444"
-          course="Mattematisk statistik och discret mattematik"
-        />
+      <Fade in={checked}>
         <Box
+          sx={{
+            height: '80%',
+            width: '100%',
+            backgroundColor: 'white',
+            position: 'absolute',
+            borderRadius: 8,
+          }}
           display="flex"
-          flexGrow={2}
-          borderRadius="8px"
-          sx={{ backgroundColor: 'white', padding: '40px' }}
-          marginLeft="20px"
+          justifyContent="center"
         >
-          {/* RIGHT Container */}
-          <BookInformationInput />
-        </Box>
-      </Box>
+          <Button sx={{ position: 'absolute', right: 0 }}>x</Button>
 
+          <BookInformation
+            name="hej"
+            edition="hej2"
+            year="1882"
+            ISBN="11111112222"
+            course="hej"
+          />
+        </Box>
+      </Fade>
       {/* Step 3, Describe quality */}
     </Box>
   );
