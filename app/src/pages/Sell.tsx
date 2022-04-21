be748d7ee2e4bded1b451d37329d669324a23e60
Daniel Rygaard <danryg@student.chalmers.se>
2022-04-21 13:38:45 +0200

    Added a stepper to the wizard

--- app/src/pages/Sell.tsx
+++ app/src/pages/Sell.tsx
@@ -8,6 +8,9 @@ import {
   TextField,
   Checkbox,
   FormControlLabel,
+  Stepper,
+  Step,
+  StepLabel,
 } from '@mui/material';
 import BookInformation from '../components/BookInformation';
 
@@ -21,6 +24,7 @@ const startedWindow = (
   </Stack>
 );
 
+const steps = ['Find your book', 'Check information', 'Specify condition'];
 /**
  * Selling page, in this window the user is able to upload a book to the selling page
  * @returns Sell page
@@ -28,6 +32,15 @@ const startedWindow = (
 export default function Sell() {
   const [started, setStarted] = React.useState(false);
   const [checked, setChecked] = React.useState(false);
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+  /* 
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  }; */
 
   const checkInformationWindow = (
     <Box
@@ -55,7 +68,10 @@ export default function Sell() {
           disabled={checked}
           variant="contained"
           size="large"
-          onClick={() => setChecked(true)}
+          onClick={() => {
+            setChecked(true);
+            handleNext();
+          }}
         >
           Continue
         </Button>
@@ -76,6 +92,17 @@ export default function Sell() {
           for us to know wich book you want to sell
         </Typography>
       </Box>
+      <Stepper activeStep={activeStep}>
+        {steps.map((label) => {
+          const stepProps: { completed?: boolean } = {};
+          return (
+            // eslint-disable-next-line react/jsx-props-no-spreading
+            <Step key={label} {...stepProps}>
+              <StepLabel>{label}</StepLabel>
+            </Step>
+          );
+        })}
+      </Stepper>
       {/** Wizard */}
       <Stack direction="row" width="100%" spacing={2}>
         {/** Search ISBN */}
@@ -92,12 +119,16 @@ export default function Sell() {
           <Button
             size="large"
             variant="contained"
-            onClick={() => setStarted(true)}
+            onClick={() => {
+              setStarted(true);
+              handleNext();
+            }}
             disabled={started}
           >
             {'Get started ->'}
           </Button>
         </Stack>
+
         {/** Check information */}
         <Grow in={started}>{checkInformationWindow}</Grow>
 
