de51f7b3d7844da3bceca60b85a18011c57aff0e
Daniel Rygaard <danryg@student.chalmers.se>
2022-04-21 18:35:33 +0200

    Tested cermona client and started to integrate it into the autocomplete component

--- app/src/components/SearchBook.tsx
+++ app/src/components/SearchBook.tsx
@@ -1,38 +1,38 @@
 import React from 'react';
-import { Autocomplete, Box, TextField, Typography } from '@mui/material';
-
-interface SearchBookProps {
-  bookSearchHandler: Function;
-}
+import { Box, Typography } from '@mui/material';
+import { Book } from 'cremona/dist/Book';
+import CremonaClient from 'cremona';
 
 /**
  * This component fixes an autocomplete with books and fires a handler when a bbok is autocompleted
  * @param bookSearchHandler (String) => {....} Handles when a book is searched
  * @returns SearchBook component
  */
-export default function SearchBook({ bookSearchHandler }: SearchBookProps) {
-  const [books] = React.useState([
-    { name: 'Mattematisk statistik', thing: 'hej1' },
-    { name: 'Linjär algebra', thing: 'hej2' },
-    { name: 'Descret mattematik', thing: 'hej2' },
-    { name: 'University physics', thing: 'hej2' },
-    { name: 'Maskinorienterad programmering', thing: 'hej2' },
-    { name: 'Java, steg för steg', thing: 'hej2' },
-    { name: 'ABOUT FACE', thing: 'hej2' },
-    { name: 'Shed, a guide for life', thing: 'hej2' },
-    { name: 'Mattlabb intro 200000 sidor', thing: 'hej2' },
-    { name: 'Sjuka grabbar', thing: 'hej2' },
-  ]);
-  /**
-   * Returns an array of objects that are in the autocomplete
-   */
-  const options = books.map((option) => {
-    const firstLetter = option.name[0].toUpperCase();
-    return {
-      firstLetter: /[0-9]/.test(firstLetter) ? '0-9' : firstLetter,
-      ...option,
+export default function SearchBook() {
+  const [open] = React.useState(false);
+  const [options, setOptions] = React.useState<readonly Book[]>([]);
+  const loading = open && options.length === 0;
+
+  React.useEffect(() => {
+    let active = true;
+
+    if (!loading) {
+      return undefined;
+    }
+
+    (async () => {
+      const client = new CremonaClient();
+      const ding = await client.getBooks();
+
+      if (active) {
+        setOptions([...ding]);
+      }
+    })();
+
+    return () => {
+      active = false;
     };
-  });
+  }, [loading]);
 
   return (
     <Box
@@ -45,22 +45,28 @@ export default function SearchBook({ bookSearchHandler }: SearchBookProps) {
       <Typography variant="h4" marginRight={5}>
         Step 1:
       </Typography>
-      <Autocomplete
+      {/* <Autocomplete
         id="grouped-books"
-        options={options.sort(
-          (a, b) => -b.firstLetter.localeCompare(a.firstLetter),
+        options={
+          const copy = [...options];
+          copy.sort(
+          (a, b) => -b.name.firstLetter.localeCompare(a.name.firstLetter),
         )}
-        groupBy={(option) => option.firstLetter}
+        isOptionEqualToValue={(option, value) => option.name === value.name}
+        getOptionLabel={(option) => option.name}
+        options={options}
+        loading={loading}
+        groupBy={(option) => option.name.firstLetter}
         getOptionLabel={(option) => option.name}
         sx={{ width: 300 }}
         renderInput={(params) => (
           // eslint-disable-next-line react/jsx-props-no-spreading
-          <TextField {...params} label="Write ISBN-number" />
+          <TextField {...params} label="Write ISBN-number" InputProps={{}} />
         )}
         onInputChange={(event, newInputValue) => {
           bookSearchHandler(newInputValue);
         }}
-      />
+      /> */}
     </Box>
   );
 }
