38953ba2b658dbbf6ddaa543f7c35a384519612b
Daniel Rygaard <danryg@student.chalmers.se>
2022-04-27 09:32:44 +0200

    Added bookInformationInput over the selling page

--- app/src/pages/SellingPage/BookInformationInput.tsx
+++ app/src/pages/SellingPage/BookInformationInput.tsx
@@ -3,27 +3,56 @@ import { Box, Button, Stack, TextField, Typography } from '@mui/material';
 
 export default function BookInformationInput() {
   return (
-    <Box display="flex" flexDirection="column" width="100%">
-      <Typography variant="h3">Book Information</Typography>
+    <Box
+      position="absolute"
+      left={0}
+      top={0}
+      display="flex"
+      flexDirection="column"
+      width="100vw"
+      height="100vh"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Box
+        position="absolute"
+        width="100vw"
+        height="100vh"
+        bgcolor="lightgray"
+        sx={{ opacity: 0.3 }}
+      />
       <Stack
-        direction="column"
-        spacing={4}
-        margin={8}
-        marginLeft={0}
-        sx={{ width: '100%' }}
+        width="60vw"
+        height="90vh"
+        bgcolor="white"
+        padding="30px"
+        borderRadius={2}
       >
-        <TextField id="outlined-basic" label="Name" variant="outlined" />
-        <TextField id="outlined-basic" label="Edition" variant="outlined" />
-        <TextField id="outlined-basic" label="Year" variant="outlined" />
-        <TextField id="outlined-basic" label="ISBN-Number" variant="outlined" />
-        <TextField id="outlined-basic" label="Course" variant="outlined" />
-        <TextField
-          id="outlined-basic"
-          label="Short describtion of the quality"
-          variant="outlined"
-        />
+        <Typography variant="h3">Book Information</Typography>
+        <Stack
+          direction="column"
+          spacing={4}
+          margin={8}
+          marginLeft={0}
+          sx={{ width: '100%' }}
+        >
+          <TextField id="outlined-basic" label="Name" variant="outlined" />
+          <TextField id="outlined-basic" label="Edition" variant="outlined" />
+          <TextField id="outlined-basic" label="Year" variant="outlined" />
+          <TextField
+            id="outlined-basic"
+            label="ISBN-Number"
+            variant="outlined"
+          />
+          <TextField id="outlined-basic" label="Course" variant="outlined" />
+          <TextField
+            id="outlined-basic"
+            label="Short describtion of the quality"
+            variant="outlined"
+          />
+        </Stack>
+        <Button>Upload image</Button>
       </Stack>
-      <Button>Upload image</Button>
     </Box>
   );
 }
