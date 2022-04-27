d5c87b95c217775d85520a141f0b65521d23c788
EliasFalk <elias.falk@hotmail.com>
2022-04-27 15:25:39 +0200

    add signup page

---
+++ app/src/pages/signup/SignUp.tsx
@@ -0,0 +1,206 @@
+import {
+  Alert,
+  Avatar,
+  Box,
+  Button,
+  Container,
+  CssBaseline,
+  FormControl,
+  Grid,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  Snackbar,
+  TextField,
+  Typography,
+} from '@mui/material';
+import { useLocation, useNavigate } from 'react-router-dom';
+import HowToRegIcon from '@mui/icons-material/HowToReg';
+import React, { useState } from 'react';
+import { VisibilityOff, Visibility } from '@mui/icons-material';
+import { useAuth } from '../../hooks/FBAuthProvider';
+
+export default function SignUp() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { signup } = useAuth();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [open, setOpen] = useState(true);
+  // TODO: do something with error, ideally match each error to each input field. Current solution with snackbar kind of trash.
+  const [error, setError] = useState('');
+
+  const allMandaotryFieldsFilled =
+    email && password && confirmPassword && firstName && lastName;
+
+  const from = (location.state as any)?.from?.pathname || '/';
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (password !== confirmPassword) {
+      setError('Passwords do not match');
+      return;
+    }
+    try {
+      await signup(email, password);
+    } catch (err) {
+      setError((err as Error).message);
+      return;
+    }
+    navigate(from, { replace: true });
+  };
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>,
+  ) => {
+    event.preventDefault();
+  };
+
+  const handleClose = (
+    event?: React.SyntheticEvent | Event,
+    reason?: string,
+  ) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      {error && (
+        <Snackbar
+          open={open}
+          autoHideDuration={15000}
+          onClose={handleClose}
+          anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        >
+          <Alert onClose={handleClose} severity="error" sx={{ width: '100%' }}>
+            {error}
+          </Alert>
+        </Snackbar>
+      )}
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <HowToRegIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <Grid container spacing={1} height="auto">
+            <Grid item xs>
+              <TextField
+                margin="normal"
+                required
+                id="first_name"
+                label="First Name"
+                name="first_name"
+                autoComplete="first_name"
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs>
+              <TextField
+                margin="normal"
+                required
+                id="last_name"
+                label="Last name"
+                name="last_name"
+                autoComplete="last_name"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                autoFocus
+              />
+            </Grid>
+          </Grid>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            autoFocus
+          />
+          <FormControl sx={{ mt: 1 }} variant="outlined" fullWidth required>
+            <InputLabel htmlFor="outlined-adornment-password">
+              Password
+            </InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              type={showPassword ? 'text' : 'password'}
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={() => setShowPassword(!showPassword)}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
+            />
+          </FormControl>
+          <FormControl sx={{ mt: 1 }} variant="outlined" fullWidth required>
+            <InputLabel htmlFor="outlined-adornment-password">
+              Confirm Password
+            </InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-password"
+              type={showPassword ? 'text' : 'password'}
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={() => setShowPassword(!showPassword)}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Confirm Password"
+            />
+          </FormControl>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            disabled={!allMandaotryFieldsFilled}
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign up
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+}
