51e5ea85bf2fd264fcb5703dcd77f3801ae92866
emilsvennesson <emilsvennesson@users.noreply.github.com>
2022-04-27 10:35:52 +0200

    add publisher & edition; fix authors parsing

--- cremona/src/Book.ts
+++ cremona/src/Book.ts
@@ -1,43 +1,58 @@
 export class Book {
-    readonly name: string;
-    readonly uid: number;
-    readonly articleNumber: string;
-    readonly description: string | null;
-    readonly price: number;
-    readonly url: string[];
-    readonly image: string;
-    readonly courseCodes: string[];
-    readonly authors: string[];
-    readonly isbn: number | null;
-    readonly weight: number;
-    readonly year: number | null;
+  readonly name: string;
+  readonly uid: number;
+  readonly articleNumber: string;
+  readonly description: string | null;
+  readonly price: number;
+  readonly url: string[];
+  readonly image: string;
+  readonly courseCodes: string[];
+  readonly authors: string[];
+  readonly isbn: number | null;
+  readonly weight: number;
+  readonly year: number | null;
+  readonly publisher: string | null;
+  readonly edition: number | null;
 
-    constructor(props: { [key: string]: any }) {
-        this.name = props.name.sv;
-        this.uid = props.uid;
-        this.articleNumber = props.articleNumber;
-        try {
-            this.description = props.description.sv.replace(/(<([^>]+)>)/gi, ' ') // strip out HTML tags
-        } catch (error) {
-            this.description = null;
-        }
-        this.price = props.price.current.SEK;
-        this.url = props.url;
-        this.image = props.images[0];
-        props['attributes'][1424] !== undefined ? // convert string of course codes to list
-            this.courseCodes = props['attributes'][1424]['sv'].replace(/ /g, '').split(',') :
-            this.courseCodes = [];
-        props['attributes'][1357] !== undefined ?
-            this.authors = props['attributes'][1357]['sv'].replace(', ', ',').split(',') :
-            this.authors = [];
-        props['attributes'][1422] !== undefined ?
-            this.year = parseInt(props['attributes'][1422]['sv']) :
-            this.year = null;
-        if (props.ean)
-            this.isbn = parseInt(props.ean);
-        else
-            this.isbn = null;
-        this.weight = props.weight;
+  constructor(props: { [key: string]: any }) {
+    this.name = props.name.sv;
+    this.uid = props.uid;
+    this.articleNumber = props.articleNumber;
+    try {
+      this.description = props.description.sv.replace(/(<([^>]+)>)/gi, ' '); // strip out HTML tags
+    } catch (error) {
+      this.description = null;
     }
+    this.price = props.price.current.SEK;
+    this.url = props.url;
+    this.image = props.images[0];
+    if (props['attributes'][1357] !== undefined) {
+      this.authors = props['attributes'][1357]['sv']
+        .replace(', ', ',')
+        .split(',');
+    } else if (props['attributes'][2513] !== undefined) {
+      this.authors = props['attributes'][2513]['sv']
+        .replace(', ', ',')
+        .split(',');
+    } else {
+      this.authors = [];
+    }
+    props['attributes'][1424] !== undefined // convert string of course codes to list
+      ? (this.courseCodes = props['attributes'][1424]['sv']
+          .replace(/ /g, '')
+          .split(','))
+      : (this.courseCodes = []);
+    props['attributes'][1422] !== undefined
+      ? (this.year = parseInt(props['attributes'][1422]['sv']))
+      : (this.year = null);
+    if (props.ean) this.isbn = parseInt(props.ean);
+    else this.isbn = null;
+    this.weight = props.weight;
+    props['attributes'][1420] !== undefined
+      ? (this.publisher = props['attributes'][1420]['sv'])
+      : (this.publisher = null);
+    props['attributes'][1421] !== undefined
+      ? (this.edition = parseInt(props['attributes'][1421]['sv']))
+      : (this.edition = null);
+  }
 }
-
