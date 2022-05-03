e41ac3f58501dce91d727ac94a5ad0fa0c53a92c
emilsvennesson <emilsvennesson@users.noreply.github.com>
2022-05-03 10:29:08 +0200

    update routesconfig

--- app/src/RoutesConfig.tsx
+++ app/src/RoutesConfig.tsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Article from './pages/Article';
 import AuthTest from './pages/AuthTest';
 import CreateAd from './pages/CreateAd';
-import Home from './pages/Home';
+import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import SignUp from './pages/signup/SignUp';
 
