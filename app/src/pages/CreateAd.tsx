682225cc953874a5cd8ff3771f34d26a0159ff5a
Daniel Rygaard <danryg@student.chalmers.se>
2022-05-03 11:08:27 +0200

    Changed interface of an ad and changed the neme from adAd to publishAd

--- app/src/pages/CreateAd.tsx
+++ app/src/pages/CreateAd.tsx
@@ -4,20 +4,15 @@ import AdService, { Ad } from '../services/AdService';
 
 export default function CreateAd() {
   const [ad] = useState<Ad>({
-    adDescription: 'i love pelle',
-    adPrice: 420,
-    authors: ['Pelle'],
-    bookDescription: 'i love pelle description book',
-    bookName: '',
-    courseCodes: [],
-    tags: [],
-    publishDate: new Date(),
-    wear: 0,
-    isbn: 0,
+    uid: '11111',
+    bookId: '12332',
+    price: 300,
+    condition: 'new',
+    conditionDescribtion: ' just as new',
   });
 
   const submitAd = () => {
-    AdService.addAd(ad).then((res) => {
+    AdService.publishAd(ad).then((res) => {
       // eslint-disable-next-line no-console
       console.log('ad added', res);
     });
--- app/src/services/AdService.ts
+++ app/src/services/AdService.ts
@@ -4,20 +4,15 @@ import db from '../firebase/db';
 import ServiceSuccessResponse from './ServiceSuccessResponse';
 
 export interface Ad {
-  adDescription: string;
-  adPrice: number;
-  authors: string[];
-  bookDescription: string;
-  bookName: string;
-  courseCodes: string[];
-  tags: string[];
-  publishDate: Date;
-  wear: number;
-  isbn: number;
+  uid: string;
+  bookId: string;
+  price: number;
+  condition: string;
+  conditionDescribtion: string;
 }
 
 export default class AdService {
-  static async addAd(ad: Ad): Promise<ServiceSuccessResponse> {
+  static async publishAd(ad: Ad): Promise<ServiceSuccessResponse> {
     try {
       const docRef = await addDoc(collection(db, 'ads'), ad);
       console.log('Document written with ID: ', docRef.id);
