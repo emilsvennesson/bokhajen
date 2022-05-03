bdc7832794b1b61d19b4fcb495f6abc083baae36
EliasFalk <elias.falk@hotmail.com>
2022-05-03 17:00:24 +0200

    add min length to search query

--- app/src/services/SearchService.ts
+++ app/src/services/SearchService.ts
@@ -1,10 +1,20 @@
 import { Book, CremonaClient } from 'cremona';
 
+const client = new CremonaClient();
+console.log('SearchService.ts');
+const minLength = 3;
+
 export default class SearchService {
   static async search(query: string): Promise<Book[]> {
-    const client = new CremonaClient();
-    const books = await client.search(query);
-    client.close();
+    if (query.length < minLength) {
+      return [];
+    }
+    let books: Book[] = [];
+    try {
+      books = await client.search(query);
+    } catch (e) {
+      console.log(e);
+    }
     return books;
   }
 }
