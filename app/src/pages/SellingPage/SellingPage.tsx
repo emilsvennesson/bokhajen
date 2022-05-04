582852361b52c8654673517adf44204426215e8f
DanielRygaard <72791196+Danryg@users.noreply.github.com>
2022-05-04 15:34:51 +0200

    Fixed small styling

--- app/src/pages/SellingPage/SellingPage.tsx
+++ app/src/pages/SellingPage/SellingPage.tsx
@@ -171,7 +171,7 @@ export default function SellingPage() {
   );
 
   return (
-    <Box>
+    <Box bgcolor="#C4C4C4" height="800px">
       <Snackbar open={error.open} autoHideDuration={6000} onClose={closeError}>
         <Alert severity="error" onClose={closeError}>
           {' '}
