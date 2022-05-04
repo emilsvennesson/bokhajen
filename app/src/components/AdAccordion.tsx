6fbe095771715c9979b4bf33226736b79dffc3be
Albin Sundström <albinsundstrom@hotmail.com>
2022-05-04 14:07:28 +0200

    add refactor adAccordion

---
+++ app/src/components/AdAccordion.tsx
@@ -0,0 +1,127 @@
+import {
+  Container,
+  Accordion,
+  AccordionSummary,
+  Avatar,
+  Stack,
+  Typography,
+  AccordionDetails,
+  Box,
+  Divider,
+  Button,
+} from '@mui/material';
+import React, { useState } from 'react';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MenuBookTwoToneIcon from '@mui/icons-material/MenuBookTwoTone';
+import ContactPageRoundedIcon from '@mui/icons-material/ContactPageRounded';
+import ChatBubbleTwoToneIcon from '@mui/icons-material/ChatBubbleTwoTone';
+import { mainTheme } from '../theme';
+
+interface Props {
+  ad: string;
+}
+
+export default function AdAccordion({ ad }: Props) {
+  const [expanded, setExpanded] = useState(false);
+
+  return (
+    <Container key={ad}>
+      <Accordion
+        sx={{
+          marginTop: 1,
+          '&:hover': {
+            bgcolor: mainTheme.palette.secondary.light,
+          },
+        }}
+      >
+        <AccordionSummary
+          onClick={() => setExpanded(!expanded)}
+          expandIcon={<ExpandMoreIcon sx={{ marginLeft: 1 }} />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Avatar
+            sx={{ margin: 'auto' }}
+            alt="Remy Sharp"
+            src="../assets/images/bok.png"
+          />
+
+          <Stack sx={{ marginLeft: 2 }}>
+            <Typography
+              variant="h6"
+              sx={{ width: '20%', flexShrink: 0, marginBottom: 1 }}
+            >
+              {ad}
+            </Typography>
+
+            <Stack direction="row" spacing={1} sx={{ margin: 'auto' }}>
+              {!expanded && (
+                <Typography
+                  variant="body1"
+                  sx={{
+                    display: 'flex',
+                    justifyContent: 'start',
+                    margin: 'auto',
+                  }}
+                >
+                  Condition
+                </Typography>
+              )}
+            </Stack>
+          </Stack>
+
+          <Typography
+            sx={{
+              marginRight: 1,
+              display: 'flex',
+              justifyContent: 'end',
+              flexGrow: 1,
+              margin: 'auto',
+            }}
+          >
+            Pris kr
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Stack direction="column">
+            <Box sx={{ display: 'flex', flex: 5 }}>
+              <Stack direction="column">
+                <Stack direction="row">
+                  <MenuBookTwoToneIcon sx={{ marginRight: 1 }} />
+                  <Typography>Condition</Typography>
+                </Stack>
+                <Stack direction="row" sx={{ marginTop: 1 }}>
+                  <ChatBubbleTwoToneIcon sx={{ marginRight: 1 }} />
+                  <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Suspendisse malesuada lacus ex, sit amet blandit leo
+                    lobortis eget.
+                  </Typography>
+                </Stack>
+              </Stack>
+            </Box>
+            <Divider sx={{ margin: 1 }} />
+            <Box
+              sx={{
+                display: 'flex',
+                flex: 3,
+                border: 0,
+                justifyContent: 'center',
+                margin: 'auto',
+                marginBottom: 0,
+                marginTop: 1,
+              }}
+            >
+              <Button
+                variant="contained"
+                startIcon={<ContactPageRoundedIcon />}
+              >
+                Kontakta säljaren
+              </Button>
+            </Box>
+          </Stack>
+        </AccordionDetails>
+      </Accordion>
+    </Container>
+  );
+}
--- app/src/pages/bookdetailview/BookDetailViewAds.tsx
+++ app/src/pages/bookdetailview/BookDetailViewAds.tsx
@@ -1,26 +1,15 @@
 import {
-  AccordionSummary,
-  AccordionDetails,
   Typography,
   SelectChangeEvent,
   FormControl,
   InputLabel,
   MenuItem,
   Select,
-  Accordion,
   Box,
-  Avatar,
-  Button,
-  Stack,
-  Divider,
   Container,
 } from '@mui/material';
 import { useState } from 'react';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MenuBookTwoToneIcon from '@mui/icons-material/MenuBookTwoTone';
-import ContactPageRoundedIcon from '@mui/icons-material/ContactPageRounded';
-import ChatBubbleTwoToneIcon from '@mui/icons-material/ChatBubbleTwoTone';
-import { mainTheme } from '../../theme';
+import AdAccordion from '../../components/AdAccordion';
 
 function BookDetailViewAds() {
   const [ads] = useState([
@@ -39,104 +28,6 @@ function BookDetailViewAds() {
   const handleChange = (event: SelectChangeEvent) => {
     setSort(event.target.value as string);
   };
-
-  const createAdItem = (ad: any) => (
-    <Container key={ad}>
-      <Accordion
-        sx={{
-          marginTop: 1,
-          '&:hover': {
-            bgcolor: mainTheme.palette.secondary.light,
-          },
-        }}
-      >
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon sx={{ marginLeft: 1 }} />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Avatar
-            sx={{ margin: 'auto' }}
-            alt="Remy Sharp"
-            src="../assets/images/bok.png"
-          />
-
-          <Stack sx={{ marginLeft: 2 }}>
-            <Typography
-              variant="h6"
-              sx={{ width: '20%', flexShrink: 0, marginBottom: 1 }}
-            >
-              {ad}
-            </Typography>
-
-            <Stack direction="row" spacing={1} sx={{ margin: 'auto' }}>
-              <Typography
-                variant="body1"
-                sx={{
-                  display: 'flex',
-                  justifyContent: 'start',
-                  margin: 'auto',
-                }}
-              >
-                Condition
-              </Typography>
-            </Stack>
-          </Stack>
-
-          <Typography
-            sx={{
-              marginRight: 1,
-              display: 'flex',
-              justifyContent: 'end',
-              flexGrow: 1,
-              margin: 'auto',
-            }}
-          >
-            Pris kr
-          </Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Stack direction="column">
-            <Box sx={{ display: 'flex', flex: 5 }}>
-              <Stack direction="column">
-                <Stack direction="row">
-                  <MenuBookTwoToneIcon sx={{ marginRight: 1 }} />
-                  <Typography>Condition</Typography>
-                </Stack>
-                <Stack direction="row" sx={{ marginTop: 1 }}>
-                  <ChatBubbleTwoToneIcon sx={{ marginRight: 1 }} />
-                  <Typography>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Suspendisse malesuada lacus ex, sit amet blandit leo
-                    lobortis eget.
-                  </Typography>
-                </Stack>
-              </Stack>
-            </Box>
-            <Divider sx={{ margin: 1 }} />
-            <Box
-              sx={{
-                display: 'flex',
-                flex: 3,
-                border: 0,
-                justifyContent: 'center',
-                margin: 'auto',
-                marginBottom: 0,
-                marginTop: 1,
-              }}
-            >
-              <Button
-                variant="contained"
-                startIcon={<ContactPageRoundedIcon />}
-              >
-                Kontakta säljaren
-              </Button>
-            </Box>
-          </Stack>
-        </AccordionDetails>
-      </Accordion>
-    </Container>
-  );
 
   return (
     <Container
@@ -187,7 +78,9 @@ function BookDetailViewAds() {
         </Box>
       </Container>
 
-      {ads.map((ad: any) => createAdItem(ad))}
+      {ads.map((ad) => (
+        <AdAccordion ad={ad} />
+      ))}
     </Container>
   );
 }
