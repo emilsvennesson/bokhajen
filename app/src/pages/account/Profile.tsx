60aed010934999758b09b248e091017b385ee74a
Pouya <kexon@hotmail.se>
2022-05-04 13:38:19 +0200

    Created profile class

---
+++ app/src/pages/account/Profile.tsx
@@ -0,0 +1,9 @@
+import { Container, Typography } from '@mui/material';
+
+export default function Profile() {
+  return (
+    <Container>
+      <Typography variant="h1">Welcome noob!</Typography>
+    </Container>
+  );
+}
