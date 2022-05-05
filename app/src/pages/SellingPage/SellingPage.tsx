93529a12e2e34969ee2df81212df63074978893f
Daniel Rygaard <danryg@student.chalmers.se>
2022-05-05 15:37:48 +0200

    The wizard card windows are now their own components

--- app/src/pages/SellingPage/SellingPage.tsx
+++ app/src/pages/SellingPage/SellingPage.tsx
@@ -2,31 +2,26 @@ import React, { useEffect } from 'react';
 import {
   Box,
   Stack,
-  Typography,
-  Button,
-  Grow,
   Stepper,
   Step,
   StepLabel,
   Snackbar,
   Alert,
-  Paper,
-  Card,
-  TextField,
 } from '@mui/material';
-import EastOutlinedIcon from '@mui/icons-material/EastOutlined';
-import KeyboardBackspaceOutlinedIcon from '@mui/icons-material/KeyboardBackspaceOutlined';
+
 import { Book } from 'cremona/dist/Book';
 import { useNavigate } from 'react-router-dom';
-import SearchBook from '../../components/SearchBook';
-import CheckInformationCard from './CheckInformationCard';
-import ConditionCheckCard from './ConditionCheckCard';
+
 import BookInformationInput from './BookInformationInput';
 import Conditions from '../../config/Conditions';
 import AdService from '../../services/AdService';
 import { NewAdvert } from '../../services/Advert';
 import { useAuth } from '../../hooks/FBAuthProvider';
 import OverlayCircularProgress from '../../components/OverlayCircularProgress';
+import SearchForBookWindowCard from './Wizard/SearchForBookWindowCard';
+import CheckInformationWIndow from './Wizard/CheckInformationWIndow';
+import ConditionCheckWindow from './Wizard/ConditionCheckWindow';
+import SetPriceWindow from './Wizard/SetPriceWindow';
 
 const steps = [
   'Find your book',
@@ -121,144 +116,6 @@ export default function SellingPage() {
         displayError(`Error:${res.data.error}`);
       });
   };
-  const searchForBookWindow = (
-    <Box flexGrow={1}>
-      <Paper elevation={5}>
-        <Stack
-          bgcolor="white"
-          alignItems="center"
-          spacing={5}
-          padding={2}
-          paddingTop={5}
-          borderRadius={2}
-          height="335px"
-        >
-          <Typography textAlign="center" variant="h2">
-            Get started
-          </Typography>
-          <SearchBook
-            disabled={activeStep > 0}
-            bookSearchHandler={(inBook: Book) => setBook(inBook)}
-          />
-          <Button
-            size="large"
-            variant="contained"
-            onClick={() => {
-              handleNext();
-            }}
-            disabled={activeStep !== 0 || book === undefined}
-            endIcon={<EastOutlinedIcon />}
-          >
-            Get started
-          </Button>
-        </Stack>
-      </Paper>
-    </Box>
-  );
-
-  const checkInformationWindow = (
-    <Box flexGrow={1.5} height="300px">
-      <Paper elevation={5}>
-        <CheckInformationCard
-          book={book}
-          backButtonHandler={() => {
-            handleBack();
-          }}
-          disabled={activeStep !== 1 || edit}
-          continueButtonHandler={() => handleNext()}
-          editButtonHandler={() => setEdit(true)}
-        />
-      </Paper>
-    </Box>
-  );
-
-  const conditionCheckWindow = (
-    <Box flexGrow={3}>
-      <Paper elevation={5}>
-        <ConditionCheckCard
-          backButtonHandler={() => handleBack()}
-          nextButtonHandler={(incondition: string, inDescribtion: string) => {
-            handleNext();
-            setCondition(incondition);
-            setdescribtion(inDescribtion);
-          }}
-          disabled={activeStep === 3}
-        />
-      </Paper>
-    </Box>
-  );
-
-  const setPriceWindow = (
-    <Box width="600px">
-      <Paper elevation={5}>
-        <Box padding={1}>
-          {/* Back button */}
-          <Box flexShrink={20} height="100%">
-            <Button onClick={() => handleBack()} variant="contained">
-              <KeyboardBackspaceOutlinedIcon />
-            </Button>
-          </Box>
-          <Stack
-            height="200px"
-            bgcolor="white"
-            zIndex={1}
-            borderRadius={2}
-            alignItems="center"
-            justifyContent="center"
-            direction="row"
-            paddingBottom="30px"
-            spacing={3}
-          >
-            <Stack spacing={2} alignItems="center" flexShrink={20}>
-              {/* Price reference cards */}
-              <Box flexShrink={20}>
-                <Card>
-                  <Stack
-                    direction="row"
-                    alignItems="center"
-                    spacing="20px"
-                    padding={2}
-                  >
-                    <Box
-                      component="img"
-                      width="90px"
-                      src="https://shop.textalk.se/shop/22777/files/Logotyper/Logga%20vit%20botten.png?max-width=600&max-height=120&quality=85"
-                    />
-                    <Typography variant="h5" fontWeight="bold">
-                      Chalmers Store
-                    </Typography>
-                    <Box display="flex">
-                      <Typography variant="h5">{book?.price}</Typography>
-                      <Typography variant="h5">:-</Typography>
-                    </Box>
-                  </Stack>
-                </Card>
-              </Box>
-              {/* Price textfield */}
-              <Box flexGrow={1}>
-                <TextField
-                  type="number"
-                  label="price"
-                  onChange={(value) => setPrice(+value.target.value)}
-                />
-              </Box>
-
-              {/* Continue button */}
-              <Box flexGrow={1} flexShrink={20}>
-                <Button
-                  onClick={() => handleDone()}
-                  variant="contained"
-                  size="large"
-                >
-                  Continue
-                </Button>
-              </Box>
-            </Stack>
-          </Stack>
-        </Box>
-      </Paper>
-    </Box>
-  );
 
   return (
     <Box height="500px">
@@ -296,18 +153,40 @@ export default function SellingPage() {
         {/** Wizard */}
         <Stack direction="row" width="100%" spacing={1}>
           {/** Search ISBN */}
-          {searchForBookWindow}
+          <SearchForBookWindowCard
+            setBook={setBook}
+            handleNext={handleNext}
+            active={book === undefined || activeStep === 0}
+          />
           {/** Check information */}
-          <Grow in={book != null && activeStep > 0}>
-            {checkInformationWindow}
-          </Grow>
+          <CheckInformationWIndow
+            book={book}
+            handleBack={handleBack}
+            handleNext={handleNext}
+            setEdit={setEdit}
+            show={!edit && activeStep > 0}
+          />
 
           {/** Set quality */}
-          <Grow in={activeStep > 1 && book != null}>
-            {conditionCheckWindow}
-          </Grow>
+
+          <ConditionCheckWindow
+            handleNext={handleNext}
+            handleBack={handleBack}
+            show={activeStep > 1 && book != null}
+            setCondition={setCondition}
+            setDescription={setdescribtion}
+            active={activeStep === 2}
+          />
         </Stack>
-        <Grow in={book != null && activeStep > 2}>{setPriceWindow}</Grow>
+
+        <SetPriceWindow
+          book={book}
+          handleBack={handleBack}
+          handleNext={handleDone}
+          setPrice={setPrice}
+          show={book != null && activeStep > 2}
+          active={activeStep === 3}
+        />
       </Stack>
       <BookInformationInput
         book={book}
---
+++ app/src/pages/SellingPage/Wizard/SearchForBookWindowCard.tsx
@@ -0,0 +1,52 @@
+import { Box, Paper, Stack, Typography, Button } from '@mui/material';
+import { Book } from 'cremona';
+import React from 'react';
+import EastOutlinedIcon from '@mui/icons-material/EastOutlined';
+import SearchBook from '../../../components/SearchBook';
+
+interface SearchForBookWindowCardProps {
+  setBook: Function;
+  handleNext: Function;
+  active: boolean;
+}
+
+export default function SearchForBookWindowCard({
+  setBook,
+  handleNext,
+  active,
+}: SearchForBookWindowCardProps) {
+  return (
+    <Box flexGrow={1}>
+      <Paper elevation={5}>
+        <Stack
+          bgcolor="white"
+          alignItems="center"
+          spacing={5}
+          padding={2}
+          paddingTop={5}
+          borderRadius={2}
+          height="335px"
+        >
+          <Typography textAlign="center" variant="h2">
+            Get started
+          </Typography>
+          <SearchBook
+            disabled={!active}
+            bookSearchHandler={(inBook: Book) => setBook(inBook)}
+          />
+          <Button
+            size="large"
+            variant="contained"
+            onClick={() => {
+              handleNext();
+            }}
+            disabled={!active}
+            endIcon={<EastOutlinedIcon />}
+          >
+            Get started
+          </Button>
+        </Stack>
+      </Paper>
+    </Box>
+  );
+}
