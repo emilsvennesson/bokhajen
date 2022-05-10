269b91083ba1f6c04e7732b937ef2a24e7e51afc
Kexon <kexon@hotmail.se>
2022-05-10 15:45:19 +0200

    Reworked homepage call to action and first iteration of it

--- app/src/pages/home/Home.tsx
+++ app/src/pages/home/Home.tsx
@@ -1,7 +1,7 @@
-import { Box } from '@mui/material';
+import { Box, Button, Grid, Stack, Typography } from '@mui/material';
 import { Book, CremonaClient } from 'cremona';
 import { Fragment, useEffect, useState } from 'react';
-import bok from '../../assets/images/bok.png';
+import shark from '../../assets/images/shark.png';
 import CategoriesGrid from './CategoriesGrid';
 
 const CATEGORIES = ['Populär studentlitteratur', 'Matematik', 'Susvetenskap'];
@@ -31,14 +31,47 @@ export default function Home() {
       >
         <Box
           component="header"
-          width="100%"
-          height={350}
+          maxWidth="lg"
           display="flex"
           justifyContent="center"
           alignItems="center"
-          bgcolor="#DFDDDD"
+          sx={{ border: 1 }}
         >
-          <Box component="img" height={250} src={bok} />
+          <Grid
+            container
+            sx={{ border: 1, borderColor: 'red', marginTop: '50px' }}
+          >
+            <Grid item xs={12} md={6} sx={{ border: 1, borderColor: 'blue' }}>
+              <Box width="100%" display="flex" justifyContent="center">
+                <Box component="img" sx={{ width: '80%' }} src={shark} />
+              </Box>
+            </Grid>
+            <Grid item xs={12} md={6} sx={{ border: 1, borderColor: 'blue' }}>
+              <Box sx={{ padding: '20px', border: 1, borderColor: 'green' }}>
+                <Stack spacing={3}>
+                  <Typography variant="h4" sx={{ fontWeight: 'bold' }}>
+                    Köp & sälj kurslitteratur på ett smartare sätt
+                  </Typography>
+                  <Typography variant="h6">
+                    Köp dina kursböcker direkt från andra studenter och spara
+                    uppåt 70%. När du är klar säljer du enkelt vidare böckerna
+                    och tjänar pengar på nolltid.
+                  </Typography>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    sx={{
+                      width: '300px',
+                      fontSize: '30px',
+                      alignSelf: 'center',
+                    }}
+                  >
+                    Sälj
+                  </Button>
+                </Stack>
+              </Box>
+            </Grid>
+          </Grid>
         </Box>
       </Box>
       <Box component="main">
