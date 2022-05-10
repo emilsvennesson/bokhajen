15098727b860f6418d848cd07672b076a56bb80b
Kexon <kexon@hotmail.se>
2022-05-10 16:37:35 +0200

    Moved the code to a different class

--- app/src/pages/home/Home.tsx
+++ app/src/pages/home/Home.tsx
@@ -1,8 +1,8 @@
-import { Box, Button, Grid, Stack, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 import { Book, CremonaClient } from 'cremona';
 import { Fragment, useEffect, useState } from 'react';
-import shark from '../../assets/images/shark.png';
 import CategoriesGrid from './CategoriesGrid';
+import HomeBanner from './HomeBanner';
 
 const CATEGORIES = ['Populär studentlitteratur', 'Matematik', 'Susvetenskap'];
 const GRID_SIZE = 12;
@@ -21,66 +21,14 @@ export default function Home() {
   }, []);
 
   return (
-    <>
-      <Box
-        component="div"
-        display="flex"
-        flexDirection="column"
-        justifyContent="center"
-        alignItems="center"
-      >
-        <Box
-          component="header"
-          maxWidth="lg"
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-        >
-          <Grid container sx={{ marginTop: '50px' }}>
-            <Grid item xs={12} md={6}>
-              <Box width="100%" display="flex" justifyContent="center">
-                <Box component="img" sx={{ width: '80%' }} src={shark} />
-              </Box>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <Box sx={{ marginTop: '55px', padding: 2 }}>
-                <Stack spacing={2}>
-                  <Typography variant="h4" sx={{ fontWeight: 'bold' }}>
-                    Köp & sälj kurslitteratur på ett smartare sätt
-                  </Typography>
-                  <Typography variant="h6">
-                    Köp dina kursböcker direkt från andra studenter och spara
-                    uppåt 70%. När du är klar säljer du enkelt vidare böckerna
-                    och tjänar pengar på nolltid.
-                  </Typography>
-                  <Grid container display="flex">
-                    <Grid item xs={12} sm={6} sx={{ padding: 1 }}>
-                      <Button
-                        variant="contained"
-                        color="secondary"
-                        size="large"
-                        fullWidth
-                      >
-                        Köp böcker
-                      </Button>
-                    </Grid>
-                    <Grid item xs={12} sm={6} sx={{ padding: 1 }}>
-                      <Button
-                        variant="contained"
-                        color="secondary"
-                        size="large"
-                        fullWidth
-                      >
-                        Sälj böcker
-                      </Button>
-                    </Grid>
-                  </Grid>
-                </Stack>
-              </Box>
-            </Grid>
-          </Grid>
-        </Box>
-      </Box>
+    <Box
+      component="div"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <HomeBanner />
       <Box component="main">
         {CATEGORIES.map((name: string) => {
           const carousel = (
@@ -95,6 +43,6 @@ export default function Home() {
           return carousel;
         })}
       </Box>
-    </>
+    </Box>
   );
 }
---
+++ app/src/pages/home/HomeBanner.tsx
@@ -0,0 +1,61 @@
+import { Box, Button, Grid, Stack, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
+import shark from '../../assets/images/shark.png';
+
+export default function HomeBanner() {
+  return (
+    <Box
+      component="header"
+      maxWidth="lg"
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Grid container sx={{ marginTop: '50px' }}>
+        <Grid item xs={12} md={6}>
+          <Box width="100%" display="flex" justifyContent="center">
+            <Box component="img" sx={{ width: '80%' }} src={shark} />
+          </Box>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Box sx={{ marginTop: '55px', padding: 2 }}>
+            <Stack spacing={2}>
+              <Typography variant="h4" sx={{ fontWeight: 'bold' }}>
+                Köp & sälj kurslitteratur på ett smartare sätt
+              </Typography>
+              <Typography variant="h6">
+                Köp dina kursböcker direkt från andra studenter och spara uppåt
+                70%. När du är klar säljer du enkelt vidare böckerna och tjänar
+                pengar på nolltid.
+              </Typography>
+              <Grid container display="flex">
+                <Grid item xs={12} sm={6} sx={{ padding: 1 }}>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    size="large"
+                    fullWidth
+                  >
+                    Köp böcker
+                  </Button>
+                </Grid>
+                <Grid item xs={12} sm={6} sx={{ padding: 1 }}>
+                  <Button
+                    component={Link}
+                    to="/sell"
+                    variant="contained"
+                    color="secondary"
+                    size="large"
+                    fullWidth
+                  >
+                    Sälj böcker
+                  </Button>
+                </Grid>
+              </Grid>
+            </Stack>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
