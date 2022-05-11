414e644b04a6d7fb8cae60ae7c23fad8c376b4fd
Pouya <kexon@hotmail.se>
2022-05-11 10:25:49 +0200

    fixed routesConfig

--- app/src/RoutesConfig.tsx
+++ app/src/RoutesConfig.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import AccountAds from './pages/account/AccountAds';
 import AccountDetails from './pages/account/AccountDetails';
 import AccountProfile from './pages/account/AccountProfile';
@@ -15,15 +15,16 @@ export default function RoutesConfig() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/article" element={<Article />} />
-      <Route path="authtest" element={<AuthTest />} />
-      <Route path="login" element={<Login />} />
-      <Route path="signup" element={<SignUp />} />
+      <Route path="/authtest" element={<AuthTest />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/account" element={<Navigate to="details" />} />
       <Route path="/account" element={<AccountProfile />}>
         <Route path="details" element={<AccountDetails />} />
         <Route path="ads" element={<AccountAds />} />
       </Route>
-      <Route path="sell" element={<SellingPage />} />
-      <Route path="*" element={<InvalidPage />} />
+      <Route path="/sell" element={<SellingPage />} />
+      <Route path="/*" element={<InvalidPage />} />
     </Routes>
   );
 }
