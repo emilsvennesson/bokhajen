526ec749e23423737c51a1adf6e7117ac1ef2578
emilsvennesson <emilsvennesson@users.noreply.github.com>
2022-05-12 22:50:03 +0200

    setup props for account ads accordion

---
+++ app/src/pages/account/AccountAdsAccordion.tsx
@@ -0,0 +1,33 @@
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { Advert } from '../../services/Advert';
+
+interface Props {
+  ads?: Advert[];
+}
+
+export default function AccountAdsAccordion({ ads }: Props) {
+  console.log(ads);
+  return (
+    <div>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>sussy little boy</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+}
