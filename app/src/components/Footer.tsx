bd7ebc6cf358d639eda1139c5a003e5fed54fcd4
Albin Sundström <albinsundstrom@hotmail.com>
2022-05-16 14:42:58 +0200

    add container to align footer better

--- app/src/components/Footer.tsx
+++ app/src/components/Footer.tsx
@@ -1,4 +1,12 @@
-import { Box, Grid, IconButton, Link, Stack, Typography } from '@mui/material';
+import {
+  Box,
+  Container,
+  Grid,
+  IconButton,
+  Link,
+  Stack,
+  Typography,
+} from '@mui/material';
 import React from 'react';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import { blueGrey } from '@mui/material/colors';
@@ -10,55 +18,56 @@ export default function Footer() {
         bottom: 0,
         width: '100%',
         bgcolor: blueGrey[700],
-        pt: '3em',
-        pb: '3em',
+        py: '3em',
       }}
     >
-      <Grid container spacing="3em" sx={{ pr: '3em', pl: '3em' }}>
-        <Grid item xs={12} sm={4}>
-          <Stack>
-            <Box borderBottom={1} marginBottom={1} color="lightgrey">
-              Kontakta oss
+      <Container>
+        <Grid container spacing="3em" sx={{ px: '3em' }}>
+          <Grid item xs={12} sm={4}>
+            <Stack>
+              <Box borderBottom={1} marginBottom={1} color="lightgrey">
+                Kontakta oss
+              </Box>
+              <Typography color="lightgrey">
+                Café Bulten, Hörsalsvägen 7, 412 58 Göteborg
+              </Typography>
+              <Link
+                component="a"
+                href="mailto:bokhajen@gmail.com"
+                color="lightgrey"
+                underline="hover"
+              >
+                bokhajen@gmail.com
+              </Link>
+            </Stack>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Stack>
+              <Box borderBottom={1} marginBottom={1} color="lightgrey">
+                Läs mer
+              </Box>
+              <Link href="/omoss" underline="hover" color="lightgrey">
+                Om oss
+              </Link>
+              <Link href="/regler&villkor" underline="hover" color="lightgrey">
+                Regler & Villkor
+              </Link>
+            </Stack>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Box borderBottom={1} marginBottom={1} color="lightGrey">
+              Länkar
             </Box>
-            <Typography color="lightgrey">
-              Café Bulten, Hörsalsvägen 7, 412 58 Göteborg
-            </Typography>
-            <Link
-              component="a"
-              href="mailto:bokhajen@gmail.com"
-              color="lightgrey"
-              underline="hover"
+            <IconButton
+              onClick={() => {
+                window.open('https://github.com/emilsvennesson/dat257-xzibit');
+              }}
             >
-              bokhajen@gmail.com
-            </Link>
-          </Stack>
+              <GitHubIcon sx={{ fontSize: '40px', color: 'black' }} />
+            </IconButton>
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={4}>
-          <Stack>
-            <Box borderBottom={1} marginBottom={1} color="lightgrey">
-              Läs mer
-            </Box>
-            <Link href="/omoss" underline="hover" color="lightgrey">
-              Om oss
-            </Link>
-            <Link href="/regler&villkor" underline="hover" color="lightgrey">
-              Regler & Villkor
-            </Link>
-          </Stack>
-        </Grid>
-        <Grid item xs={12} sm={4}>
-          <Box borderBottom={1} marginBottom={1} color="lightGrey">
-            Länkar
-          </Box>
-          <IconButton
-            onClick={() => {
-              window.open('https://github.com/emilsvennesson/dat257-xzibit');
-            }}
-          >
-            <GitHubIcon sx={{ fontSize: '40px', color: 'black' }} />
-          </IconButton>
-        </Grid>
-      </Grid>
+      </Container>
     </Box>
   );
 }
