7a72b03de5a06e498aea6a774210b9b287fd8468
EliasFalk <elias.falk@hotmail.com>
2022-05-20 11:24:14 +0200

    add active styling to current menu item in account nav

--- app/src/pages/account/AccountNavigation.tsx
+++ app/src/pages/account/AccountNavigation.tsx
@@ -1,18 +1,47 @@
 import { Button, Stack } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { createStyles, makeStyles } from '@mui/styles';
+import { forwardRef } from 'react';
+
+const MyNavLink = forwardRef<any, any>((props, ref) => (
+  <NavLink
+    ref={ref}
+    to={props.to}
+    className={({ isActive }) =>
+      `${props.className} ${isActive ? props.activeClassName : ''}`
+    }
+  >
+    {props.children}
+  </NavLink>
+));
+
+const useStyles = makeStyles(() =>
+  createStyles({
+    activeLink: {
+      fontWeight: 'bold',
+      fontSize: '16px',
+    },
+  }),
+);
 
 export default function AccountNavigation() {
+  const classes = useStyles();
   return (
     <Stack>
       <Button
-        component={Link}
+        component={MyNavLink}
         to="details"
         size="large"
-        sx={{ fontWeight: 'bold', fontSize: '16px' }}
+        activeClassName={classes.activeLink}
       >
         Mina uppgifter
       </Button>
-      <Button component={Link} to="ads" size="large">
+      <Button
+        component={MyNavLink}
+        to="ads"
+        size="large"
+        activeClassName={classes.activeLink}
+      >
         Mina annonser
       </Button>
     </Stack>
